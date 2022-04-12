d2974ecf62f3040dde19640f32afa242d58c81d7
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-04-12 21:55:09 +0200

    edit movies works

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const Movie = require("../models/Movies.model");
 const Celebrity = require("../models/Celebrity.model");
-const { render } = require("../app");
+
 
 router.get('/movies', (req, res, next) => {
     
@@ -30,19 +30,33 @@ router.get('/movies/create', (req, res, next) => {
 
 router.get('/movies/:id/edit', (req, res, next) => {
     const id = req.params.id;
-    Movie.findById( id )
-    .then(movie => {
-        res.render('movies/edit-movie', { movie })
-        console.log(movie)
-        //console.log(movie.cast[0])
-        //Celebrity.find({ id: movie.cast[0]})
+    let celebrities;
+    Celebrity.find()
+    .then((celebritiesFromDB) => {
+        celebrities = celebritiesFromDB;
+       return Movie.findById( id )
     })
-    .then(()=> {})
+    .then(movie => {
+        res.render('movies/edit-movie', { movie, celebrities })
+        console.log(movie)
+    })
     .catch(error=> {
         next(error)
     })
 })
 
+router.post('/movies/:id/edit', (req, res, next)=> {
+    const id = req.params.id;
+    const { title, genre, plot, cast } = req.body;
+    Movie.findByIdAndUpdate( id , {title, genre, plot, cast}, { new:true} )
+    .then(newMovie => {
+        console.log(newMovie)
+        res.redirect('/movies')
+    })
+    .catch(error => {
+        next(error)
+    })
+})
 
 router.get('/movies/:id', (req, res, next) => {
     const id = req.params.id;
