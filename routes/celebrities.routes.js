ce4367e58e174d090b2703d916ec650d7574db32
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-04-12 09:45:42 +0200

    iteration 2-4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,27 @@
+const Celebrity = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
 // all your routes here
+router.get('/celebrities/create', (req, res) => {
+    res.render('celebrities/new-celebrity')
+})
+
+router.post('/celebrities/create', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    
+    Celebrity.create({ name, occupation, catchPhrase })
+    .then((celebrity) => {
+        res.render('celebrities/celebrities')
+        console.log("Name: "+ celebrity.name)
+    })
+    .catch((error)=> {
+        console.log(error);
+        res.redirect('celebrities/new-celebrity')
+    })
+    .next(error => {
+        next(error)
+    })
+})
 
 module.exports = router;
